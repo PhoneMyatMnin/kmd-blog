23de6609476dc65d961a05addbb0638f97aea992
phonemyatmin <phonemyatminofficial@gmail.com>
2024-03-28 09:45:50 +0630

    a

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -23,7 +23,6 @@
             <div class="col-md-3"></div>
             <div class="col-md-6">
                 <h5>Create Post</h5>
-                Hello Bitch!
                 <form action="{{ url('posts') }}" method="POST">
                     @csrf
                     <div class="form-group">
