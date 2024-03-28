d208c582ad39259e370b1f664d978ca142c11d4c
Ivan <wailintun@gusto.edu.mm>
2024-03-28 09:48:27 +0630

    new lable in create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -23,7 +23,6 @@
             <div class="col-md-3"></div>
             <div class="col-md-6">
                 <h5>Create Post</h5>
-                Skie Bitch
                 <form action="{{ url('posts') }}" method="POST">
                     @csrf
                     <div class="form-group">
